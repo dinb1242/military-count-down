38b5e13b47b21c708bbcc2cafc49ff9954660c66
Jihyun, Jeong <dinb1242@naver.com>
2022-10-27 22:24:26 +0900

    GraphQL Config 를 AppModule 내에 정의

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      // debug: false,
+      // playground: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
