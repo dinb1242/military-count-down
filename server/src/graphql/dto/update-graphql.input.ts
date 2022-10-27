5db61ae124a820b50547ed8c53731d938dfb3a80
Jihyun, Jeong <dinb1242@naver.com>
2022-10-27 22:24:07 +0900

    GraphQL 관련 디렉토리 및 하위 파일 제거

--- server/src/graphql/dto/update-graphql.input.ts
+++
@@ -1,6 +0,0 @@
-import { CreateGraphqlInput } from './create-graphql.input';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateGraphqlInput extends PartialType(CreateGraphqlInput) {
-  id: number;
-}
