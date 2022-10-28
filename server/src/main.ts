acfb64ded707c03c7d9456ede10e834cac95015d
JiHyun, Jeong <39480072+dinb1242@users.noreply.github.com>
2022-10-28 11:40:35 +0900

    서버 프로젝트 main.ts 내 dotenv 환경 별 분리 로직 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as path from 'path';
+import { Logger } from '@nestjs/common';
+
+import dotenv = require('dotenv');
+dotenv.config();
+
+if (process.env.NODE_ENV === 'local') {
+  Logger.log('서버가 로컬 환경에서 동작합니다.');
+  dotenv.config({ path: path.join(__dirname, '../.env-local') });
+} else if (process.env.NODE_ENV === 'dev') {
+  Logger.log('서버가 개발 환경에서 동작합니다.');
+  dotenv.config({ path: path.join(__dirname, '../.env-dev') });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
