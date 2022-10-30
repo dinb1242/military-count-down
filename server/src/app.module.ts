660b2c864f63fb9564b5a9580a263559d2dcc52a
Jihyun, Jeong <dinb1242@naver.com>
2022-10-30 22:13:02 +0900

    GraphQL Module 제거

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
-      // debug: false,
-      // playground: false,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
