35bbd96a42d3705beafb03e29dcd8dddcac30f02
JiHyun, Jeong <39480072+dinb1242@users.noreply.github.com>
2022-11-01 17:16:09 +0900

    class-transformer 를 이용한 글로벌 인터셉터 및 파이프를 엔트리 포인트에 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as path from 'path';
-import { Logger } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
-import dotenv = require('dotenv');
-dotenv.config();
-
-if (process.env.NODE_ENV === 'local') {
-  Logger.log('서버가 로컬 환경에서 동작합니다.');
-  dotenv.config({ path: path.join(__dirname, '../.env-local') });
-} else if (process.env.NODE_ENV === 'dev') {
-  Logger.log('서버가 개발 환경에서 동작합니다.');
-  dotenv.config({ path: path.join(__dirname, '../.env-dev') });
-}
+// import dotenv = require('dotenv');
+// dotenv.config();
+//
+// if (process.env.NODE_ENV === 'local') {
+//   Logger.log('서버가 로컬 환경에서 동작합니다.');
+//   dotenv.config({ path: path.join(__dirname, '../.env-local') });
+// } else if (process.env.NODE_ENV === 'dev') {
+//   Logger.log('서버가 개발 환경에서 동작합니다.');
+//   dotenv.config({ path: path.join(__dirname, '../.env-dev') });
+// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(8080);
 }
+
 bootstrap();
