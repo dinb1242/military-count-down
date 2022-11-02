b2e0b351830ff02fb7c248c3f11654d5f2a50ffa
JiHyun, Jeong <39480072+dinb1242@users.noreply.github.com>
2022-11-02 15:39:46 +0900

    Example Response DTO 생성

---
+++ server/src/example/dto/response/example-response.dto.ts
@@ -0,0 +1,12 @@
+import { Example } from '../../entities/example.entity';
+
+export class ExampleResponseDto {
+  constructor(entity: Example) {
+    this.id = entity.id;
+    this.title = entity.title;
+    this.content = entity.content;
+  }
+  private readonly id: number;
+  private readonly title: string;
+  private readonly content: string;
+}
