3c33a635c30be9da929ae68b542e15fc782ddb36
Jihyun, Jeong <dinb1242@naver.com>
2022-11-07 22:50:29 +0900

    Interceptor, Filter 등의 어플리케이션 관련 파일 common 패키지로 이동에 따른 Import 위치 변경

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from './interceptors/response.interceptor';
-import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { ResponseInterceptor } from './commons/interceptors/response.interceptor';
+import { HttpExceptionFilter } from './commons/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
