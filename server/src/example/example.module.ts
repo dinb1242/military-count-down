80c74dd8957197a4e1c69124857cbdfb9e013ca6
Jihyun, Jeong <dinb1242@naver.com>
2022-11-07 22:48:30 +0900

    TypeORM 커스텀 DatabaseModule 생성

--- server/src/example/example.module.ts
+++ server/src/example/example.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ExampleService } from './example.service';
 import { ExampleController } from './example.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Example } from './entities/example.entity';
-// import { exampleProviders } from './providers/example.providers';
-import { DatabaseModule } from '../database/database.module';
+import { DatabaseModule } from '../commons/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
