28bb0cc548602bd221409c23bf748a0ec4665ce5
JiHyun, Jeong <39480072+dinb1242@users.noreply.github.com>
2022-11-11 17:49:10 +0900

    main.ts 내 스웨거 콘피그 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { PrismaService } from './common/prisma/prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,8 +12,19 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  // 스웨거 설정
+  const config = new DocumentBuilder()
+    .setTitle('멋쟁이 명세 문서 구축 전투')
+    .setDescription('모든 남조선 개발자 인민들은 미제국주의의 멋쟁이 명세 문서를 낱낱이 파악하라.')
+    .setVersion('1.11')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 
