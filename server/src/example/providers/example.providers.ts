ea77aaeb6069fa9082839a7e72cc35a200b0db0d
Jihyun, Jeong <dinb1242@naver.com>
2022-11-13 22:39:05 +0900

    Example 엔티티 및 Provider 제거

--- server/src/example/providers/example.providers.ts
+++
@@ -1,7 +0,0 @@
-// export const exampleProviders = [
-//   {
-//     provide: REPOSITORIES.EXAMPLE_REPOSITORY,
-//     useFactory: (dataSource: DataSource) => dataSource.getRepository(Example),
-//     inject: [DATA_SOURCE],
-//   },
-// ];
