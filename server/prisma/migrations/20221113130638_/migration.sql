91e256bd211db57d6d1f722df7d5003dbb0a5b2a
Jihyun, Jeong <dinb1242@naver.com>
2022-11-13 22:15:35 +0900

    Prisma Schema 내 Example 스키마 생성 및 Migrate

---
+++ server/prisma/migrations/20221113130638_/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Example" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Example_pkey" PRIMARY KEY ("id")
+);
