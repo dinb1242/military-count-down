600b8ed0d0b390a2ec2ee1e261b795165958a033
Jihyun, Jeong <dinb1242@gmail.com>
2022-11-16 17:53:46 +0900

    cipher.util.ts 내 hashMatches 메소드 추가

--- server/src/common/utils/cipher.util.ts
+++ server/src/common/utils/cipher.util.ts
@@ -6,26 +6,30 @@ const encKey = process.env.ENC_KEY;
 
 @Injectable()
 export class CipherUtils {
-    async hash(plainText: string): Promise<string> {
-        const saltRounds = 10;
-        return await bcrypt.hash(plainText, saltRounds);
-    }
+  async hash(plainText: string): Promise<string> {
+    const saltRounds = 10;
+    return await bcrypt.hash(plainText, saltRounds);
+  }
 
-    async encodeByAES56(data: string): Promise<string>{
-        const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(encKey), {
-            iv: CryptoJS.enc.Utf8.parse(""),
-            padding: CryptoJS.pad.Pkcs7,
-            mode: CryptoJS.mode.CBC
-        });
-        return cipher.toString();
-    }
+  async hashMatches(plainText: string, hashedText: string): Promise<boolean> {
+    return await bcrypt.compare(plainText, hashedText);
+  }
 
-    async decodeByAES256(data: string): Promise<string> {
-        const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(encKey), {
-            iv: CryptoJS.enc.Utf8.parse(""),
-            padding: CryptoJS.pad.Pkcs7,
-            mode: CryptoJS.mode.CBC
-        });
-        return cipher.toString(CryptoJS.enc.Utf8);
-    };
+  async encodeByAES56(data: string): Promise<string> {
+    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(encKey), {
+      iv: CryptoJS.enc.Utf8.parse(''),
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC,
+    });
+    return cipher.toString();
+  }
+
+  async decodeByAES256(data: string): Promise<string> {
+    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(encKey), {
+      iv: CryptoJS.enc.Utf8.parse(''),
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC,
+    });
+    return cipher.toString(CryptoJS.enc.Utf8);
+  }
 }
