a4d529ecc2529a442c421cf9c2d86cd4902e522c
Jihyun, Jeong <dinb1242@naver.com>
2022-11-20 22:56:58 +0900

    Jwt Guard 전역 적용 및 Public 데코레이터 생성, 관련 핸들러에 적용

--- server/src/auth/guards/jwt-auth.guard.ts
+++ server/src/auth/guards/jwt-auth.guard.ts
@@ -1,20 +1,26 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
-import { SignInUnauthorizedException } from '../../common/exceptions/sign-in-unauthorized.exception';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/auth-public.decorator';
 
 /**
  * JWT 인증이 필요한 핸들러에 사용되는 Guard
  */
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    return super.canActivate(context);
+  constructor(private reflector: Reflector) {
+    super();
   }
 
-  handleRequest(err: any, user: any) {
-    if (err !== null || !user) throw new SignInUnauthorizedException();
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
-    return user;
+    if (isPublic) return true;
+
+    return super.canActivate(context);
   }
 }
