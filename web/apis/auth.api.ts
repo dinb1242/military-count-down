7c22b4685707195f80c77eb7c8eaa09eca098740
Jihyun, Jeong <dinb1242@naver.com>
2022-12-01 21:22:01 +0900

    로그아웃 구현

--- web/apis/auth.api.ts
+++ web/apis/auth.api.ts
@@ -5,7 +5,11 @@ export default {
     return axiosInstance.post('/auth/sign-in', data);
   },
 
+  signOut() {
+    return axiosInstance.post('/auth/sign-out');
+  },
+
   checkToken() {
     return axiosInstance.post('/auth/check', null);
   }
-}
+};
