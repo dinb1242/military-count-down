fad0d99bc74a85324ac715e284e9c80bbfe5d582
Jihyun, Jeong <dinb1242@gmail.com>
2022-12-02 15:21:54 +0900

    [Frontend] Sign-in Spin 구현 및 SignIn 페이지에 적용

---
+++ web/components/spinning/loading.spin.tsx
@@ -0,0 +1,9 @@
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+
+export const LoadingSpin = () => {
+  return (
+    <>
+      <AiOutlineLoading3Quarters className={ "animate-spin w-8 h-8" }/>
+    </>
+  )
+}
--- web/pages/auth/sign-in.tsx
+++ web/pages/auth/sign-in.tsx
@@ -1,9 +1,10 @@
 import { HiArrowCircleRight, HiUserCircle } from "react-icons/hi";
 import Link from "next/link";
-import { ChangeEvent, useState } from "react";
-import AuthApi from '../../apis/auth.api';
-import { useRouter } from 'next/router';
-import AlertModal from '../../components/modals/alert.modal';
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import AuthApi from "../../apis/auth.api";
+import { useRouter } from "next/router";
+import AlertModal from "../../components/modals/alert.modal";
+import { LoadingSpin } from "../../components/spinning/loading.spin";
 
 export const SignIn = () => {
   const router = useRouter();
@@ -17,13 +18,15 @@ export const SignIn = () => {
   const [isAccountEmpty, setIsAccountEmpty] = useState({
     email: false,
     password: false,
-  })
+  });
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleModal = () => {
     setIsModalOpen((a) => !a);
-  }
+  };
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.currentTarget;
@@ -32,36 +35,37 @@ export const SignIn = () => {
       [name]: value,
     });
 
-    if (name === 'email' && value !== '')
+    if (name === "email" && value !== "")
       setIsAccountEmpty({
         ...isAccountEmpty,
         email: false,
-      })
-    else if (name === 'password' && value !== '')
+      });
+    else if (name === "password" && value !== "")
       setIsAccountEmpty({
         ...isAccountEmpty,
         password: false,
-      })
+      });
   };
 
   const handleSignInClick = async () => {
+    setIsLoading(true);
     const { email, password } = inputs;
 
-    if (email === '' && password === '') {
+    if (email === "" && password === "") {
       setIsAccountEmpty({
         email: true,
-        password: true
-      })
-    } else if (email === '') {
+        password: true,
+      });
+    } else if (email === "") {
       setIsAccountEmpty({
         ...isAccountEmpty,
-        email: true
-      })
-    } else if (password === '') {
+        email: true,
+      });
+    } else if (password === "") {
       setIsAccountEmpty({
         ...isAccountEmpty,
-        password: true
-      })
+        password: true,
+      });
     }
 
     await AuthApi.signIn({ email, password })
@@ -69,28 +73,39 @@ export const SignIn = () => {
         // 로그인에 성공하였다면, 토큰을 LocalStorage 에 저장한다.
         if (res.data.success) {
           const { accessToken, refreshToken } = res.data.data;
-          localStorage.setItem('Access-Token', accessToken);
-          localStorage.setItem('Refresh-Token', refreshToken);
-          router.push('/');
+          localStorage.setItem("Access-Token", accessToken);
+          localStorage.setItem("Refresh-Token", refreshToken);
+          router.push("/");
         }
-      }).catch(() => {
-        setIsModalOpen(true);
       })
-  }
+      .catch(() => {
+        setIsLoading(false);
+        setIsModalOpen(true);
+      });
+  };
+
+  const handleSignInKeyUp = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") handleSignInClick();
+  };
 
   return (
     <div
-      className={ "min-h-screen p-8 flex flex-col justify-center items-center" }
+      className={"min-h-screen p-8 flex flex-col justify-center items-center"}
     >
-      <AlertModal isOpen={ isModalOpen } handleModal={ handleModal } title={ '로그인 실패' } content={ '아이디 혹은 비밀번호가 일치하지 않습니다.' }/>
-      <div className={ "border rounded w-96 h-auto flex flex-col items-center" }>
-        <div className={ "flex flex-col items-center mt-8" }>
-          <HiUserCircle className={ "w-16 h-16 text-gray-500" }/>
-          <h1 className={ "text-2xl font-bold" }>로그인</h1>
+      <AlertModal
+        isOpen={isModalOpen}
+        handleModal={handleModal}
+        title={"로그인 실패"}
+        content={"아이디 혹은 비밀번호가 일치하지 않습니다."}
+      />
+      <div className={"border rounded w-96 h-auto flex flex-col items-center"}>
+        <div className={"flex flex-col items-center mt-8"}>
+          <HiUserCircle className={"w-16 h-16 text-gray-500"} />
+          <h1 className={"text-2xl font-bold"}>로그인</h1>
         </div>
 
-        {/* 로그인 폼 */ }
-        <div className={ "w-2/3" }>
+        {/* 로그인 폼 */}
+        <div className={"w-2/3"}>
           <label className="label">
             <span className="label-text font-bold">이메일</span>
           </label>
@@ -98,13 +113,14 @@ export const SignIn = () => {
             className={
               "input input-md input-info input-bordered w-full max-w-xs"
             }
-            name={ "email" }
-            value={ inputs.email }
-            onChange={ (event) => handleInputChange(event) }
+            name={"email"}
+            value={inputs.email}
+            onChange={(event) => handleInputChange(event)}
+            onKeyUp={(event) => handleSignInKeyUp(event)}
           />
           <div>
-            <span className={ 'text-xs text-red-500' }>
-              { isAccountEmpty.email && '아이디를 입력해주세요.' }
+            <span className={"text-xs text-red-500"}>
+              {isAccountEmpty.email && "아이디를 입력해주세요."}
             </span>
           </div>
           <label className="label">
@@ -114,25 +130,32 @@ export const SignIn = () => {
             className={
               "input input-md input-info input-bordered w-full max-w-xs"
             }
-            type={ "password" }
-            name={ 'password' }
-            value={ inputs.password }
-            onChange={ (event) => handleInputChange(event) }
+            type={"password"}
+            name={"password"}
+            value={inputs.password}
+            onChange={(event) => handleInputChange(event)}
+            onKeyUp={(event) => handleSignInKeyUp(event)}
           />
           <div>
-            <span className={ 'text-xs text-red-500' }>
-              { isAccountEmpty.password && '비밀번호를 입력해주세요.' }
+            <span className={"text-xs text-red-500"}>
+              {isAccountEmpty.password && "비밀번호를 입력해주세요."}
             </span>
           </div>
         </div>
-        <a className={ "mt-4" } onClick={ handleSignInClick }>
-          <HiArrowCircleRight
-            className={
-              "w-12 h-12 transition hover:duration-200 ease-in-out rounded-full cursor-pointer text-emerald-500 hover:scale-110 hover:-translate-y-1 hover:text-emerald-600 active:text-emerald-700 hover:duration-200"
-            }
-          />
-        </a>
-        <Link href={ "/auth/sign-up/agree" }>
+        {!isLoading ? (
+          <a className={"mt-4"} onClick={handleSignInClick}>
+            <HiArrowCircleRight
+              className={
+                "w-12 h-12 transition hover:duration-200 ease-in-out rounded-full cursor-pointer text-emerald-500 hover:scale-110 hover:-translate-y-1 hover:text-emerald-600 active:text-emerald-700 hover:duration-200"
+              }
+            />
+          </a>
+        ) : (
+          <div className={"mt-4"}>
+            <LoadingSpin />
+          </div>
+        )}
+        <Link href={"/auth/sign-up/agree"}>
           <a
             className={
               "my-4 text-blue-500 font-bold transition hover:duration-200 hover:-translate-y-1 hover:scale-110"
