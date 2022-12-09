06e7210032d736cd3826e7648b92e8f4218d0c6c
Jeong, Jihyun <dinb1242@naver.com>
2022-12-09 10:46:39 +0900

    [Backend & Frontend] Wiki 리액토링 #3 - 비즈니스 로직 작성

--- server/src/wiki/dto/response/wiki-revision-response.dto.ts
+++ server/src/wiki/dto/response/wiki-revision-response.dto.ts
@@ -1,7 +1,6 @@
-import {CoworkerWikiResponseDto} from "../../../coworker/dto/response/coworker-wiki-response.dto";
-import {UserResponseDto} from "../../../user/dto/response/user-response.dto";
-import {TimeUtils} from "../../../common/utils/time.util";
-import {ApiProperty} from "@nestjs/swagger";
+import { UserResponseDto } from "../../../user/dto/response/user-response.dto";
+import { TimeUtils } from "../../../common/utils/time.util";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class WikiRevisionResponseDto {
     constructor(wikiRevision: any) {
@@ -9,7 +8,7 @@ export class WikiRevisionResponseDto {
         this.authorId = wikiRevision.authorId;
         this.wikiId = wikiRevision.coworkerWikiId;
         this.wikiContent = wikiRevision.wikiContent;
-        this.coworkerWiki = new CoworkerWikiResponseDto(wikiRevision.coworkerWiki);
+        // this.coworkerWiki = new CoworkerWikiResponseDto(wikiRevision.coworkerWiki);
         this.author = new UserResponseDto(wikiRevision.author);
         this.createdAt = TimeUtils.convertDateToLocalDateTimeStr(wikiRevision.createdAt);
         // this.updatedAt = TimeUtils.convertDateToLocalDateTimeStr(wikiRevision.updatedAt);
@@ -27,8 +26,8 @@ export class WikiRevisionResponseDto {
     @ApiProperty({ description: '위키 마크다운 내용' })
     wikiContent: string | null;
 
-    @ApiProperty({ description: '부모 위키 DTO' })
-    coworkerWiki: CoworkerWikiResponseDto;
+    // @ApiProperty({ description: '부모 위키 DTO' })
+    // wiki: CoworkerWikiResponseDto;
 
     @ApiProperty({ description: '작성자 정보 DTO' })
     author: UserResponseDto;
@@ -36,6 +35,6 @@ export class WikiRevisionResponseDto {
     @ApiProperty({ description: '생성일' })
     createdAt: string;
 
-    @ApiProperty({ description: '수정일' })
-    updatedAt: string;
+    // @ApiProperty({ description: '수정일' })
+    // updatedAt: string;
 }
--- server/src/wiki/wiki.service.ts
+++ server/src/wiki/wiki.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../common/prisma/prisma.service';
-import { Prisma } from '@prisma/client';
+import { Prisma, WikiRevision as WikiRevisionModel } from '@prisma/client';
 import { WikiResponseDto } from './dto/response/wiki-response.dto';
-import { WikiType } from '../common/enums/wiki-type.enum';
+import { WikiRevisionResponseDto } from "./dto/response/wiki-revision-response.dto";
 
 @Injectable()
 export class WikiService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {
+  }
 
   async upsert(wikiCreateInput: Prisma.WikiCreateInput): Promise<WikiResponseDto> {
     const wikiEntity = await this.prismaService.wiki.create({
@@ -20,10 +21,22 @@ export class WikiService {
     return new WikiResponseDto(wikiEntity);
   }
 
-  async findOneWiki(bbsType: WikiType, bbsId: number, test: Prisma.WikiFindFirstArgsBase): Promise<WikiResponseDto> {
-    await this.prismaService.wiki.findFirst({
-      where: {  }
-    })
+  async findOneWiki(wikiFindFirstArgsBase: Prisma.WikiFindFirstArgsBase): Promise<WikiResponseDto> {
+    const wikiEntity = await this.prismaService.wiki.findFirst(wikiFindFirstArgsBase);
+    return new WikiResponseDto(wikiEntity);
+  }
 
+  async findAllWikiRevisions(wikiRevisionFindManyArgs: Prisma.WikiRevisionFindManyArgs): Promise<WikiRevisionResponseDto[]> {
+    const wikiRevisionEntityList: WikiRevisionModel[] = await this.prismaService.wikiRevision.findMany(wikiRevisionFindManyArgs);
+    return wikiRevisionEntityList.map(eachEntity => new WikiRevisionResponseDto(eachEntity));
+  }
+
+  async findOneWikiRevision(wikiRevisionFindUniqueOrThrowArgs: Prisma.WikiRevisionFindUniqueOrThrowArgs): Promise<WikiRevisionResponseDto> {
+    const wikiRevisionEntity = await this.prismaService.wikiRevision.findUniqueOrThrow(wikiRevisionFindUniqueOrThrowArgs)
+      .catch(() => {
+        throw new NotFoundException('일치하는 Revision 을 찾을 수 없습니다.');
+      });
+
+    return new WikiRevisionResponseDto(wikiRevisionEntity);
   }
 }
