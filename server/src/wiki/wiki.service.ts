7f8fb375fc1cc62918ddb113d5449b0351ea4986
Jihyun, Jeong <dinb1242@gmail.com>
2022-12-09 09:16:34 +0900

    [Frontend & Backend] Coworker 위키 개발 중 #2

--- server/src/wiki/wiki.service.ts
+++ server/src/wiki/wiki.service.ts
@@ -1,22 +1,29 @@
-import {Injectable} from '@nestjs/common';
-import {PrismaService} from "../common/prisma/prisma.service";
-import {Prisma} from '@prisma/client';
-import {WikiResponseDto} from "./dto/response/wiki-response.dto";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../common/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+import { WikiResponseDto } from './dto/response/wiki-response.dto';
+import { WikiType } from '../common/enums/wiki-type.enum';
 
 @Injectable()
 export class WikiService {
-    constructor(private readonly prismaServie: PrismaService) {
-    }
+  constructor(private readonly prismaService: PrismaService) {}
 
-    async upsert(wikiCreateInput: Prisma.WikiCreateInput): Promise<WikiResponseDto> {
-        const wikiEntity = await this.prismaServie.wiki.create({
-            data: wikiCreateInput,
-            include: {
-                wikiRevision: true,
-                coworker: true
-            }
-        });
+  async upsert(wikiCreateInput: Prisma.WikiCreateInput): Promise<WikiResponseDto> {
+    const wikiEntity = await this.prismaService.wiki.create({
+      data: wikiCreateInput,
+      include: {
+        wikiRevision: true,
+        coworker: true,
+      },
+    });
 
-        return new WikiResponseDto(wikiEntity);
-    }
+    return new WikiResponseDto(wikiEntity);
+  }
+
+  async findOneWiki(bbsType: WikiType, bbsId: number, test: Prisma.WikiFindFirstArgsBase): Promise<WikiResponseDto> {
+    await this.prismaService.wiki.findFirst({
+      where: {  }
+    })
+
+  }
 }
