2d4bc36e0f2e44a7c367c3b5c22b71240c9a7efb
Jeong, Jihyun <dinb1242@naver.com>
2022-12-15 11:20:58 +0900

    [DevOps] Nginx 적용 완료 - 테스트 필요

--- server/src/main.ts
+++ server/src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(8080, '0.0.0.0');
 }
