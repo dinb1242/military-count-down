3025a31f5c228f82ee5efef1b885b25f7e431994
Jeong, Jihyun <dinb1242@naver.com>
2022-12-20 15:25:50 +0900

    [Backend] JWT Guard 내 DB 토큰 크로스 체크 로직 작성

--- server/src/auth/guards/jwt-auth.guard.ts
+++ server/src/auth/guards/jwt-auth.guard.ts
@@ -1,19 +1,19 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from '../decorators/auth-public.decorator';
+import { PrismaService } from "../../common/prisma/prisma.service";
 
 /**
  * JWT 인증이 필요한 핸들러에 사용되는 Guard
  */
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(private reflector: Reflector, private readonly prismaService: PrismaService) {
     super();
   }
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
@@ -21,6 +21,21 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
 
     if (isPublic) return true;
 
-    return super.canActivate(context);
+    const isValidJwt: any = await super.canActivate(context);
+
+    if (isValidJwt) {
+      // JWT 토큰을 데이터베이스와 비교한다.
+      const request: Request = context.switchToHttp().getRequest();
+      const { authorization }: any = request.headers;
+
+      const isExistsToken = await this.prismaService.authToken.findUnique({
+        where: { accessToken: authorization.replace('Bearer ', '') }
+      })
+
+      if (!isExistsToken) {
+        return false;
+      }
+    }
+    return isValidJwt;
   }
 }
--- server/src/auth/strategy/jwt.strategy.ts
+++ server/src/auth/strategy/jwt.strategy.ts
@@ -9,7 +9,6 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly configService: ConfigService) {
-    // TODO: 자동완성 안 됨
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
